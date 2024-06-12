3177ffd58909aeaf9b18099680635109932d2f21
fantaz-st <cbabic@pfst.hr>
2024-06-12 10:52:50 +0200

    searchParams error fix attempt 2

--- src/components/CategoryLink/CategoryLink.jsx
+++ src/components/CategoryLink/CategoryLink.jsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Typography } from "@mui/material";
 
 const CategoryLink = ({ categorySlug, categoryName, searchParams }) => {
-  const router = useRouter();
-
   const createCategoryLink = (categorySlug) => {
     const newSearchParams = new URLSearchParams(searchParams);
     newSearchParams.set("category", categorySlug);
