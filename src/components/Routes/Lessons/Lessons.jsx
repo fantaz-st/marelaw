032488353b30ba2d43ad56aece93a88ae2393f54
fantaz-st <cbabic@pfst.hr>
2025-01-21 16:09:30 +0100

    Lessons sorting

--- src/components/Routes/Lessons/Lessons.jsx
+++ src/components/Routes/Lessons/Lessons.jsx
@@ -1,9 +1,23 @@
 import Link from "next/link";
 import classes from "./Lessons.module.css";
-import { Box, Button, List, ListItem, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
 
 const Lessons = ({ lessons }) => {
+  const sortedLessons = [...lessons].sort((a, b) => {
+    const extractNumbers = (str) => str.replace("lesson-", "").split(".").map(Number);
+
+    const aParts = extractNumbers(a.slug);
+    const bParts = extractNumbers(b.slug);
+
+    for (let i = 0; i < Math.max(aParts.length, bParts.length); i++) {
+      const aNum = aParts[i] || 0;
+      const bNum = bParts[i] || 0;
+      if (aNum !== bNum) return aNum - bNum;
+    }
+    return 0;
+  });
+
   return (
     <Box as='article' className={classes.container} maxWidth='xl'>
       <Box className={classes.inner}>
@@ -11,7 +25,7 @@ const Lessons = ({ lessons }) => {
           <Typography variant='h1' className={classes.title}>
             Lessons
           </Typography>
-          {lessons.map((lesson) => (
+          {sortedLessons.map((lesson) => (
             <Link href={`/lessons/${lesson.slug}`} key={lesson.slug} className={classes.lesson}>
               <Typography variant='h5' className={classes.title}>
                 {lesson.title}
