ce439847503c0a1c780557867c59dc0c8e9867b2
fantaz <cbabic@pfst.hr>
2025-04-08 13:47:44 +0200

    Page unordered list bullets

--- src/elements/BlockElements/ListBlock/ListBlock.jsx
+++ src/elements/BlockElements/ListBlock/ListBlock.jsx
@@ -2,8 +2,9 @@ import Block from "@/components/Block/Block";
 import { List } from "@mui/material";
 
 const ListBlock = ({ ordered, innerBlocks }) => {
+  // console.log(innerBlocks);
   return (
-    <List sx={{ listStyle: ordered ? "decimal" : "none" }}>
+    <List sx={{ listStyle: ordered ? "decimal" : "none", listStyleType: ordered ? "decimal" : "square" }}>
       {innerBlocks.map((block, i) => (
         <Block key={i} block={block} /* size={size} */ />
       ))}
